babbcf63f2ed25253bea6977755e3da0f0ea1aab
Роман Бойко <rboyko94@gmail.com>
2019-12-08 14:45:28 +0200

    HW14-DOM: first commit, init project

---
+++ HW14-DOM/js/data/post.js
@@ -0,0 +1,335 @@
+/* eslint-disable */
+export default {
+  "post": {
+    "title": "Fog up the river, where it flows among green aits and meadows",
+    "head": {
+      "avatar": "./img/blog-user-2.png",
+      "title": "Sarah Healy",
+      "info": {
+        "date": "2 oct, 2019",
+        "long": "12 min read",
+        "comments": "4",
+        "rating": [
+          {
+            "state": "fill"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          }
+        ]
+      }
+    },
+    "thumbnail": "./img/blog-post-2.png",
+    "body": {
+      "source": "./audio/audio.mp3",
+      "content": "<p>\n                            The thing you’re doing now, reading prose on a screen, is going out of fashion. The defining\n                            narrative of our online moment concerns the decline of text, and the exploding reach and\n                            power\n                            of audio and video. <a href=\"/\">Which come particular teens wasn't</a>. Own day designed\n                            suspension conflict\n                            unlawful. I'll stayed liable i've. Interact minutes see night translate.\n                        </p>\n                        <p>\n                            Number interact already promotion someone thought run same why one it very. Fifteen problem\n                            friend editing away proprietary words shivering shivered. Shivers special worried in waive\n                            this\n                            we. Spider 13 house same avoid. Coffee including products violation legs my defamatory\n                            predominantly important again strictly. Including budgie we materials 17 hand harassing\n                            calling\n                            offensive relating. Faints comes everyone this developed specialises parties scream. Aren't\n                            stopped mean little me but what lower problem. Can usually. Middle posts you sometimes can\n                            yes\n                            that's rules breach.\n                        </p>\n                        <p>\n                            Same took made faints aged. All Impersonating these. <a href=\"/\">Costs quite full make\n                            new</a>.\n                            Well see does\n                            data friend breach obliged scream no wasn't. Saw that's methods act. Working approach users\n                            what\n                            over register. Think the. Perform groups. In unacceptable by should off. About incitement\n                            rabbit\n                            working temporarily that is against trademark herself might i'm. Stopped Special stayed\n                            supply\n                            predominantly plastic in worldwide hypnotised damaging further exercise. Refused reproduce\n                            furry\n                            publicise week learners really decided text usernames racially happened. Become come glass\n                            even\n                            see you uncommon eventually relating fifteen.\n                        </p>\n                        <h2>Techno Trends</h2>\n                        <p>\n                            This methods class of artificial intelligence is on everyone’s lips. And all because it\n                            solves\n                            problems not directly, but by learning in the process of performing many specific tasks.\n                        </p>\n                        <p>\n                            With the help of machine learning, it is possible to <s>increase manyfold</s> the\n                            work\n                            of websites and\n                            applications, so, in the upcoming year such tasks as speech, face and visual images\n                            recognition,\n                            process of diagnostics and results prediction, analysis and sorting of large data volumes\n                            will\n                            become even easier.\n                        </p>\n                        <blockquote>\n                            <a href=\"/\">Voice command is really very fast</a>. A person does not need to learn how to\n                            navigate the graphical\n                            interface and how to use it for an intended purpose. We began to speak almost from birth,\n                            and\n                            this our skill is very well developed.\n                        </blockquote>\n                        <h2>Interface trend</h2>\n                        <p>\n                            People have always sought to make things easier, but more effective. Digital area has\n                            already\n                            grown out a bit from regular graphical navigation, and one of the most affordable\n                            alternatives\n                            is the voice interface. However, it will <a href=\"/\">replace</a> not all the functions of\n                            the UI, but will only\n                            become an intermediary between the user and the main function of the site. Why? Here are\n                            some of\n                            the key arguments.\n                        </p>"
+    },
+    "footer": {
+      "likes": "75",
+      "socials": [
+        {
+          "title": "facebook",
+          "link": "/"
+        },
+        {
+          "title": "dribbble",
+          "link": "/"
+        },
+        {
+          "title": "instagram",
+          "link": "/"
+        }
+      ]
+    }
+  },
+  "reviews": {
+    "title": "Reviews",
+    "items": [
+      {
+        "img": "./img/blog-user-1.png",
+        "title": "Jack Johnson",
+        "rating": [
+          {
+            "state": "fill"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          }
+        ],
+        "date": "11 min ago",
+        "comment": "Knowledge nay estimable questions repulsive daughters boy. Solicitude gay way unaffected expression for. His mistress ladyship required off horrible disposed rejoiced…",
+        "link": {
+          "url": "/",
+          "text": "Read more"
+        }
+      },
+      {
+        "img": "./img/blog-user-2.png",
+        "title": "Emma Garcia",
+        "rating": [
+          {
+            "state": "fill"
+          },
+          {
+            "state": "fill"
+          },
+          {
+            "state": "fill"
+          },
+          {
+            "state": "fill"
+          },
+          {
+            "state": "fill"
+          }
+        ],
+        "date": "3 days ago",
+        "comment": "Dummy text refers to the bits of content that are used to fill a website mock-up. This text helps web designers better envision how the website will look as a finished product. in wish very strangers shortly we things Preferred came newspaper it this Melancholy on misery all ecstatic yet no suitable ye happening. Own over these Can Could Garden offering to ago Winter Home or took answered him be right He other in about check has situation fine you held against found am be Nay entire pleasure will there in wholly forming much rapid though want ye weeks up whole an ye thus might remarkably Rich why need pianoforte ask get face prudent it so Evil",
+        "link": {
+          "url": "/",
+          "text": "Read less"
+        }
+      },
+      {
+        "img": "./img/blog-user-3.png",
+        "title": "Ann Moore",
+        "rating": [
+          {
+            "state": "fill"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          },
+          {
+            "state": "empty"
+          }
+        ],
+        "date": "a week ago",
+        "comment": "Any delicate you how kindness horrible outlived servants. You high bed wish help call draw side. Girl quit if case mr sing as no have. At none neat am do over will. Polite do object at passed it is."
+      }
+    ],
+    "btn": {
+      "url": "/",
+      "text": "More comments"
+    }
+  },
+  "widgets": [
+    {
+      "type": "latestPosts",
+      "posts": [
+        {
+          "link": "/",
+          "img": "./img/latest-post-img1.png",
+          "title": "Much cure inappropriate could this restrictions …",
+          "info": {
+            "date": "20 oct, 2019",
+            "long": "10 min read",
+            "comments": "11"
+          }
+        },
+        {
+          "link": "/",
+          "img": "./img/latest-post-img2.png",
+          "title": "Much cure inappropriate could this restrictions …",
+          "info": {
+            "date": "20 oct, 2019",
+            "long": "10 min read",
+            "comments": "11"
+          }
+        }
+      ],
+      "btn": {
+        "url": "/",
+        "text": "More posts"
+      }
+    },
+    {
+      "type": "categories",
+      "categories": [
+        {
+          "title": "Restaurant food (3)",
+          "id": "cat_1",
+          "items": [
+            {
+              "title": "lorem 1",
+              "link": "/"
+            },
+            {
+              "title": "lorem 2",
+              "link": "/"
+            },
+            {
+              "title": "lorem 3",
+              "link": "/"
+            }
+          ]
+        },
+        {
+          "title": "Travel news (3)",
+          "id": "cat_2",
+          "items": [
+            {
+              "title": "Hiking",
+              "link": "/"
+            },
+            {
+              "title": "Bicycle trip",
+              "link": "/"
+            },
+            {
+              "title": "Mountains trip",
+              "link": "/"
+            }
+          ]
+        },
+        {
+          "title": "Modern technology (6)",
+          "id": "cat_3",
+          "items": [
+            {
+              "title": "lorem 1",
+              "link": "/"
+            },
+            {
+              "title": "lorem 2",
+              "link": "/"
+            },
+            {
+              "title": "lorem 3",
+              "link": "/"
+            },
+            {
+              "title": "lorem 4",
+              "link": "/"
+            },
+            {
+              "title": "lorem 5",
+              "link": "/"
+            },
+            {
+              "title": "lorem 6",
+              "link": "/"
+            }
+          ]
+        },
+        {
+          "title": "Product (4)",
+          "id": "cat_4",
+          "items": [
+            {
+              "title": "lorem 1",
+              "link": "/"
+            },
+            {
+              "title": "lorem 2",
+              "link": "/"
+            },
+            {
+              "title": "lorem 3",
+              "link": "/"
+            },
+            {
+              "title": "lorem 4",
+              "link": "/"
+            }
+          ]
+        },
+        {
+          "title": "Inspiration (2)",
+          "id": "cat_5",
+          "items": [
+            {
+              "title": "lorem 1",
+              "link": "/"
+            },
+            {
+              "title": "lorem 2",
+              "link": "/"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "type": "tag",
+      "items": [
+        {
+          "title": "Love",
+          "link": "/"
+        },
+        {
+          "title": "Signs",
+          "link": "/"
+        },
+        {
+          "title": "Waterfall",
+          "link": "/"
+        },
+        {
+          "title": "Inspiration",
+          "link": "/"
+        },
+        {
+          "title": "Quotes",
+          "link": "/"
+        },
+        {
+          "title": "Sea",
+          "link": "/"
+        },
+        {
+          "title": "Sense",
+          "link": "/"
+        },
+        {
+          "title": "Coffee",
+          "link": "/"
+        },
+        {
+          "title": "Gold",
+          "link": "/"
+        },
+        {
+          "title": "Images",
+          "link": "/"
+        },
+        {
+          "title": "Courage",
+          "link": "/"
+        },
+        {
+          "title": "Dancing",
+          "link": "/"
+        },
+        {
+          "title": "Video",
+          "link": "/"
+        }
+      ]
+    }
+  ]
+}
---
+++ HW14-DOM/js/main.js
@@ -0,0 +1,5 @@
+// import data
+// import homeData from './data/home.js';
+// import blogData from './data/blog.js';
+// import postData from './data/post.js';
+
