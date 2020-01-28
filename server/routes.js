12697386f48f110dca35a1ac76aad208e91a442c
Роман Бойко <rboyko94@gmail.com>
2020-01-28 19:55:28 +0200

    HW24-Node: edit method put

--- server/routes.js
+++ server/routes.js
@@ -26,28 +26,24 @@ router.route('/articles')
     log.info("==Delete all list articles==");
     list = [];
     res.end(JSON.stringify(list));
-  })
-  .put((req, res) =>  {
-    log.info("==Put article by id==");
-    list = list.map(article => {
-      if(+article.id === +req.body.id) {
-        article.body.description = req.body.description;
-      }
-      return article;
-    });
-    res.end(JSON.stringify(list));
   });
 
 router.route('/articles/:id')
-  .delete((req, res) => {
-    log.info('==Delete article by id==');
-    list = list.filter(article => +article.id !== +req.params.id);
-    res.end(JSON.stringify(list));
-  })
   .get((req, res) =>  {
     log.info("==Get article by id==");
     const articleById = list.find(article => +article.id === +req.params.id);
     res.end(JSON.stringify(articleById));
+  })
+  .put((req, res) =>  {
+    log.info("==Put article by id==");
+    const articleIndexById = list.findIndex(article => +article.id === +req.params.id);
+    list[articleIndexById].body.description = req.body.description;
+    res.end(JSON.stringify(list));
+  })
+  .delete((req, res) => {
+    log.info('==Delete article by id==');
+    list = list.filter(article => +article.id !== +req.params.id);
+    res.end(JSON.stringify(list));
   });
 
 module.exports = router;
